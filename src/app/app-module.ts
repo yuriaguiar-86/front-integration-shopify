906f5aa8993f8c7aadaf82f1d49e1853f1ef4709
yuriaguiar-86 <kakacraft337@gmail.com>
2026-02-03 02:03:36 -0300

    (feat) importacoes e declaracoes de modulos criados

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,22 +3,30 @@ import { BrowserModule, provideClientHydration, withEventReplay } from '@angular
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
-import { ListOperation } from './pages/private/operation/list-operation/list-operation';
-import { Home } from './pages/private/home/home';
-import { FormOperation } from './pages/private/operation/form-operation/form-operation';
+import { HomeComponent } from './pages/private/home/home-component';
+import { ViewOperationComponent } from './pages/private/operation/view-operation/view-operation-component';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { ListOperationComponent } from './pages/private/operation/list-operation/list-operation-component';
+import { FormOperationComponent } from './pages/private/operation/form-operation/form-operation-component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ListProductComponent } from './pages/private/product/list-product/list-product-component';
 
 @NgModule({
   declarations: [
     App,
-    ListOperation,
-    Home,
-    FormOperation,
+    HomeComponent,
+    ViewOperationComponent,
+    ListOperationComponent,
+    FormOperationComponent,
+    ListProductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [
+    provideHttpClient(withFetch()),
     provideBrowserGlobalErrorListeners(),
     provideClientHydration(withEventReplay()),
   ],
