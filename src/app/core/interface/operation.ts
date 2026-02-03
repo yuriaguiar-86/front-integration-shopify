2095883ffc2e4a61223eca8ca89a63cdb002833e
yuriaguiar-86 <kakacraft337@gmail.com>
2026-02-03 02:01:02 -0300

    (feat) mapeamento para response e requests

---
+++ src/app/core/interface/operation.ts
@@ -0,0 +1,12 @@
+export interface Operation {
+  id: any,
+  name: string,
+
+  white_store: string,
+  white_client: string,
+  white_secret: string,
+
+  black_store: string,
+  black_client: string,
+  black_secret: string,
+}
---
+++ src/app/core/interface/shopify-access-token.ts
@@ -0,0 +1,5 @@
+export interface ShopifyAccessToken {
+  access_token: string;
+  scope?: string;
+  expires_in: number;
+}
