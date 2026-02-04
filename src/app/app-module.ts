092f207050e07b29dfe82bdd932bccf8d18c407a
yuriaguiar-86 <kakacraft337@gmail.com>
2026-02-04 15:32:33 -0300

    (feat) organizacao dos imports via tshero

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,14 +1,14 @@
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { AppRoutingModule } from './app-routing-module';
 import { HomeComponent } from './pages/private/home/home-component';
-import { ViewOperationComponent } from './pages/private/operation/view-operation/view-operation-component';
-import { provideHttpClient, withFetch } from '@angular/common/http';
-import { ListOperationComponent } from './pages/private/operation/list-operation/list-operation-component';
 import { FormOperationComponent } from './pages/private/operation/form-operation/form-operation-component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ListOperationComponent } from './pages/private/operation/list-operation/list-operation-component';
+import { ViewOperationComponent } from './pages/private/operation/view-operation/view-operation-component';
 import { ListProductComponent } from './pages/private/product/list-product/list-product-component';
 
 @NgModule({
