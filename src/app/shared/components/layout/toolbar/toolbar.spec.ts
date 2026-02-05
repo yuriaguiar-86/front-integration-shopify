a812b0bae3c3250b225abb7f6558f2acb1cbba2c
yuriaguiar-86 <kakacraft337@gmail.com>
2026-02-05 01:58:31 -0300

    (feat) componente da toolbar para paginas privadas

---
+++ src/app/shared/components/layout/toolbar/toolbar.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Toolbar } from './toolbar';
+
+describe('Toolbar', () => {
+  let component: Toolbar;
+  let fixture: ComponentFixture<Toolbar>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [Toolbar]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(Toolbar);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/shared/components/layout/toolbar/toolbar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-toolbar',
+  standalone: false,
+  templateUrl: './toolbar.html',
+  styleUrl: './toolbar.css',
+})
+export class Toolbar {
+
+}
