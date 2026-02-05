f0fdef808876d8c607d2ae638a746419a0efcc9a
yuriaguiar-86 <kakacraft337@gmail.com>
2026-02-05 00:48:18 -0300

    (feat) componente para solicitar troca de senha

---
+++ src/app/pages/public/forget-password/forget-password-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forget-password-component',
+  standalone: false,
+  templateUrl: './forget-password-component.html',
+  styleUrl: './forget-password-component.css',
+})
+export class ForgetPasswordComponent {
+
+}
