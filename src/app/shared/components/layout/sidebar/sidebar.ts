4a928ce35372953e729a332fac24777f4ccd58a5
yuriaguiar-86 <kakacraft337@gmail.com>
2026-02-05 01:58:20 -0300

    (feat) componente da sidebar para paginas privadas

---
+++ src/app/shared/components/layout/sidebar/sidebar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar',
+  standalone: false,
+  templateUrl: './sidebar.html',
+  styleUrl: './sidebar.css',
+})
+export class Sidebar {
+
+}
