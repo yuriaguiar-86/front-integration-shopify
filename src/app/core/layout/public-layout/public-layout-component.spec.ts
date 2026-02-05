4c5b5f06f53a83808a9788b5dd94efb659b4c4ce
yuriaguiar-86 <kakacraft337@gmail.com>
2026-02-05 00:46:18 -0300

    (feat) template default para paginas publicas

---
+++ src/app/core/layout/public-layout/public-layout-component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PublicLayoutComponent } from './public-layout-component';
+
+describe('PublicLayoutComponent', () => {
+  let component: PublicLayoutComponent;
+  let fixture: ComponentFixture<PublicLayoutComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [PublicLayoutComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(PublicLayoutComponent);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/core/layout/public-layout/public-layout-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-public-layout-component',
+  standalone: false,
+  templateUrl: './public-layout-component.html',
+  styleUrl: './public-layout-component.css',
+})
+export class PublicLayoutComponent {
+
+}
