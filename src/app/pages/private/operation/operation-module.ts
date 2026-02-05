c4bd3fa228f1f15bba69f8dc8e412b5f2a199404
yuriaguiar-86 <kakacraft337@gmail.com>
2026-02-05 00:47:10 -0300

    (remove) retirado importacao do component para listagem de produtos

--- src/app/pages/private/operation/operation-module.ts
+++ src/app/pages/private/operation/operation-module.ts
@@ -2,31 +2,23 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 
-import { ListProductComponent } from '../product/list-product/list-product-component';
+import { ComponentsModule } from '../../../shared/components/components-module';
 import { FormOperationComponent } from './form-operation/form-operation-component';
 import { ListOperationComponent } from './list-operation/list-operation-component';
 import { OperationRoutingModule } from './operation-routing-module';
 import { ViewOperationComponent } from './view-operation/view-operation-component';
-import { ComponentsModule } from "../../../shared/components/components-module";
 
 @NgModule({
   declarations: [
     ViewOperationComponent,
     ListOperationComponent,
-    FormOperationComponent,
-    ListProductComponent
+    FormOperationComponent
   ],
   imports: [
     CommonModule,
     OperationRoutingModule,
     ReactiveFormsModule,
     ComponentsModule
-],
-  exports: [
-    ViewOperationComponent,
-    ListOperationComponent,
-    FormOperationComponent,
-    ListProductComponent
   ],
 })
 export class OperationModule { }
