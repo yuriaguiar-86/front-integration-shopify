895a9f4795b1c5fcdc218c5e980387167e6e9e19
yuriaguiar-86 <kakacraft337@gmail.com>
2026-02-05 00:46:08 -0300

    (feat) template default para paginas privadas

---
+++ src/app/core/layout/private-layout/private-layout-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-private-layout-component',
+  standalone: false,
+  templateUrl: './private-layout-component.html',
+  styleUrl: './private-layout-component.css',
+})
+export class PrivateLayoutComponent {
+
+}
